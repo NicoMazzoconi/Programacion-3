6f35219e954a44e9b237baa7e40b21da09e2e0cf
asd <asd>
2019-05-06 22:07:49 -0300

    ApiRest

---
+++ Nueva carpeta/Ejercicio1/clases/usuario.php
@@ -0,0 +1,169 @@
+<?php
+class Usuario
+{
+	public $id;
+	public $nombre;
+	public $password;
+
+
+/* inicio  especiales para slimFramework*/
+
+ 	public function TraerUno($request, $response, $args) {
+     	$id=$args['id'];
+    	$elUsuario=Usuario::TraerUnUsuario($id);
+     	$newResponse = $response->withJson($elUsuario, 200);  
+    	return $newResponse;
+    }
+     public function TraerTodos($request, $response, $args) {
+      	$todosLosUsuarios=Usuario::TraerTodoLosUsuarios();
+     	$newResponse = $response->withJson($todosLosUsuarios, 200);  
+    	return $newResponse;
+    }
+      public function CargarUno($request, $response, $args) {
+     	$response->getBody()->write("<h1>Cargar uno nuevo</h1>");
+      	return $response;
+    }
+      public function BorrarUno($request, $response, $args) {
+     	$ArrayDeParametros = $request->getParsedBody();
+     	$id=$ArrayDeParametros['id'];
+     	$Usuario= new Usuario();
+     	$Usuario->id=$id;
+     	$cantidadDeBorrados=$Usuario->BorrarUsuario();
+
+     	$objDelaRespuesta= new stdclass();
+	    $objDelaRespuesta->cantidad=$cantidadDeBorrados;
+	    if($cantidadDeBorrados>0)
+	    	{
+	    		 $objDelaRespuesta->resultado="algo borro!!!";
+	    	}
+	    	else
+	    	{
+	    		$objDelaRespuesta->resultado="no Borro nada!!!";
+	    	}
+	    $newResponse = $response->withJson($objDelaRespuesta, 200);  
+      	return $newResponse;
+    }
+     public function ModificarUno($request, $response, $args) {
+     	//$response->getBody()->write("<h1>Modificar  uno</h1>");
+		$ArrayDeParametros = $request->getParsedBody();	
+		var_dump($ArrayDeParametros);
+	    $miUsuario = new Usuario();
+	    $miUsuario->id=$ArrayDeParametros['id'];
+	    $miUsuario->nombre=$ArrayDeParametros['nombre'];
+	    $miUsuario->password=$ArrayDeParametros['password'];
+
+	   	$resultado =$miUsuario->ModificarUsuarioParametros();
+	   	$objDelaRespuesta= new stdclass();
+		//var_dump($resultado);
+		$objDelaRespuesta->resultado=$resultado;
+		return $response->withJson($objDelaRespuesta, 200);		
+    }
+
+/* final especiales para slimFramework*/
+  	public function BorrarUsuario()
+	 {
+	 		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("
+				delete 
+				from usuarios 				
+				WHERE id=:id");	
+				$consulta->bindValue(':id',$this->id, PDO::PARAM_INT);		
+				$consulta->execute();
+				return $consulta->rowCount();
+	 }
+
+	public function ModificarUsuario()
+	 {
+
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("
+				update usuarios 
+				set nombre='$this->nombre',
+				password='$this->password',
+				WHERE id='$this->id'");
+			return $consulta->execute();
+
+	 }
+	
+  
+	 public function InsertarElUsuario()
+	 {
+				$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+				$consulta =$objetoAccesoDato->RetornarConsulta("INSERT into usuarios (nombre,password)values('$this->nombre','$this->password')");
+				$consulta->execute();
+				return $objetoAccesoDato->RetornarUltimoIdInsertado();
+				
+
+	 }
+
+	  public function ModificarUsuarioParametros()
+	 {
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("
+				update usuarios 
+				set nombre=:nombre,
+				password=:password
+				WHERE id=:id");
+			$consulta->bindValue(':id',$this->id, PDO::PARAM_INT);
+			$consulta->bindValue(':nombre',$this->nombre, PDO::PARAM_INT);
+			$consulta->bindValue(':password', $this->password, PDO::PARAM_STR);
+			return $consulta->execute();
+	 }
+
+	 public function InsertarElUsuarioParametros()
+	 {
+				$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+				$consulta =$objetoAccesoDato->RetornarConsulta("INSERT into usuarios (nombre,password)values(:nombre,:password)");
+				$consulta->bindValue(':nombre',$this->nombre, PDO::PARAM_INT);
+				$consulta->bindValue(':password', $this->password, PDO::PARAM_STR);
+				$consulta->execute();		
+				return $objetoAccesoDato->RetornarUltimoIdInsertado();
+	 }
+	 public function GuardarUsuario()
+	 {
+
+	 	if($this->id>0)
+	 		{
+	 			$this->ModificarUsuarioParametros();
+	 		}else {
+	 			$this->InsertarElUsuarioParametros();
+	 		}
+
+	 }
+
+
+  	public static function TraerTodoLosUsuarios()
+	{
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("select id,nombre,password from usuarios");
+			$consulta->execute();			
+			return $consulta->fetchAll(PDO::FETCH_CLASS, "Usuario");		
+	}
+
+	public static function TraerUnUsuario($id) 
+	{
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("select id, nombre, password from usuarios where id = $id");
+			$consulta->execute();
+			$UsuarioBuscado= $consulta->fetchObject('Usuario');
+			return $UsuarioBuscado;				
+
+			
+	}
+
+	public static function Login($nombre, $password)
+	{
+		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+		$consulta =$objetoAccesoDato->RetornarConsulta("select id, nombre, password from usuarios where nombre = ? and password = ?");
+		$consulta->execute(array($nombre, $password));
+		$UsuarioBuscado= $consulta->fetchObject('Usuario');
+		return $UsuarioBuscado;	
+	}
+
+
+	public function mostrarDatos()
+	{
+	  	return "Metodo mostar:".$this->nombre."  ".$this->password;
+	}
+
+}
